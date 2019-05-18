fb0f1045a6992463c3d9ce8c1331131b596a3eb9
Preston Hager <preston@hagerfamily.com>
2019-05-18 13:29:13 -0700

    Updated for Heroku.

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const path = require('path')
+const http = require('http');
+const enforce = require('express-sslify');
 
 const app = express()
-const port = 80
+const PORT = process.env.PORT || 80;
 
 app.use('/static', express.static(path.join(__dirname, 'static')))
 
@@ -10,4 +12,9 @@ app.get('/', (req, res) => {
   res.render('index.jade');
 })
 
-app.listen(port, () => console.log(`MealAdventure listening on port ${port}!`))
+// app.listen(port, () => console.log(`MealAdventure listening on port ${port}!`))
+app.use(enforce.HTTPS({ trustProtoHeader: true }));
+
+http.createServer(app).listen(PORT, function() {
+    console.log('MealAdventure listening on port ' + PORT);
+})
